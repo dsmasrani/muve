dd182841ef859d1dbe803b258fd4eae6e56164b7
Ryan Innamorati <rdinnamo@calpoly.edu>
2021-04-18 19:41:41 -0700

    test

--- billboardtest.py
+++ billboardtest.py
@@ -9,12 +9,12 @@ os.environ['SPOTIPY_REDIRECT_URI'] = 'http://127.0.0.1:8080/login'
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
 #playlists = sp.user_playlists('spotify')
-playlists = sp.playlist(playlist_id='37i9dQZF1DXcBWIGoYBM5M?si=Oy1No6AOSdGe2Iu27weljw', fields=None)
+playlists = sp.playlist(playlist_id='37i9dQZF1DXcBWIGoYBM5M?si=IkTBWPoyQTOT4Ppyl3j7eA', fields=None)
 songs = list()
 artists = list()
 dates = list()
 for i in range(len(playlists['tracks']['items'])):
-    if(' (' in playlists['tracks']['items'][i]['track']['name']):
+    if ' (' in playlists['tracks']['items'][i]['track']['name']:
         idx = playlists['tracks']['items'][i]['track']['name'].index(' (')
         songs.append(playlists['tracks']['items'][i]['track']['name'][:idx])
     else:
@@ -27,20 +27,38 @@ print(dates)
 newdate = list()
 bill = {}
 for i in range(len(songs)):
-    if(dates[i] not in bill.keys()):
+    if dates[i] not in bill.keys():
         bill[dates[i]] = billboard.ChartData('hot-100',date=dates[i]+'-01',fetch=True,timeout=25)
 billsongs = list()
 billartist = list()
 count = 0
+count2 = 0
+count3 = 0
 for i in range(len(songs)):
     for j in range(100):
-        billsongs.append(bill[dates[i]][j].title)
-        billartist.append(bill[dates[i]][j].artist)
-    print(songs[i] + billsongs[i])
-    print(artists[i] + billartist[i])
-    if songs[i].lower() == billsongs[i].lower() and artists[i].lower() == billartist[i].lower():
+        billsongs.append(bill[dates[i]][j].title.lower())
+        billartist.append(bill[dates[i]][j].artist.lower())
+    #print(songs[i] + billsongs[i])
+    #print(artists[i] + billartist[i])
+    #if songs[i].lower() == billsongs[i].lower() and artists[i].lower() == billartist[i].lower():
+        #count += 1
+for i in range(len(songs)):
+    if songs[i].lower() in billsongs and artists[i].lower() in billartist:
+        print(songs[i])
+        print(artists[i])
         count += 1
+    if songs[i].lower() in billsongs:
+        count2 += 1
+    if artists[i].lower() in billartist:
+        count3 += 1
+print(songs)
+print(billsongs)
 print(count)
+print(count2)
+print(count3)
+#total = len(songs)
+#print(total)
+#print(count/total)
 #print(playlists['tracks']['items'][1]['added_at'][:10])
 
 #def main():
